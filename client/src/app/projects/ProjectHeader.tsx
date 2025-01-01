fb23bb4ac353d99cafbaaa3c31c13eeb73e442ce
Khirod4300dav@gmail.com <Khirod4300dav@gmail.com>
2025-01-01 11:29:41 +0530

    minor change

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -39,7 +39,7 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
         />
       </div>
 
-    //   {/* TABS */}
+     {/* TABS */}
       <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
         <div className="flex flex-1 items-center gap-2 md:gap-4">
           <TabButton
