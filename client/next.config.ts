b18f24b281a0fe4b109b882e9d44d52322d217e6
Khirod4300dav@gmail.com <Khirod4300dav@gmail.com>
2025-05-29 13:10:22 +0530

    x3

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// next.config.ts
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // other config options...
 };
 
 export default nextConfig;
